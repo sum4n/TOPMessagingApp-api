f0286accdc62ba96a24fdf4a8ac4a4cd69b6225f
Suman Das <54246298+sum4n@users.noreply.github.com>
2026-05-23 09:41:16 +0530

    test: add test for GET /users

--- __tests__/user.test.js
+++ __tests__/user.test.js
@@ -1,11 +1,46 @@
 import userRouter from "../routes/userRouter.js";
 
 import request from "supertest";
+import { prisma } from "../lib/prisma.js";
 import express from "express";
 const app = express();
 
-app.use("/user", userRouter);
+app.use("/users", userRouter);
 
-test("user router works", (done) => {
-  request(app).get("/user").expect(200, done);
+beforeEach(async () => {
+  const users = await prisma.user.createMany({
+    data: [
+      {
+        name: "Alice",
+        email: "alice@prisma.io",
+        password: "alice",
+      },
+      {
+        name: "Bob",
+        email: "bob@prisma.io",
+        password: "bob",
+      },
+    ],
+  });
+});
+
+afterEach(async () => {
+  await prisma.user.deleteMany();
+});
+
+afterAll(async () => {
+  await prisma.$disconnect();
+});
+
+describe("GET /users", () => {
+  it("returns two users", async () => {
+    const res = await request(app)
+      .get("/users")
+      .set("Accept", "application/json");
+
+    // console.log(res.body);
+    expect(res.headers["content-type"]).toMatch(/json/);
+    expect(res.status).toBe(200);
+    expect(res.body.allUsers).toHaveLength(2);
+  });
 });
