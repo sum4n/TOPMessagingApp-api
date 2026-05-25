845c1be595650be5a9a50de4072b1ba4ae4c01b2
Suman Das <54246298+sum4n@users.noreply.github.com>
2026-05-25 05:54:26 +0530

    test: add post /users/sign-up encrypted password test

--- __tests__/user.test.js
+++ __tests__/user.test.js
@@ -2,8 +2,11 @@ import userRouter from "../routes/userRouter.js";
 
 import request from "supertest";
 import { prisma } from "../lib/prisma.js";
+import bcrypt from "bcryptjs";
 import express from "express";
 const app = express();
+
+app.use(express.json());
 
 app.use("/users", userRouter);
 
@@ -42,5 +45,20 @@ describe("GET /users", () => {
     expect(res.headers["content-type"]).toMatch(/json/);
     expect(res.status).toBe(200);
     expect(res.body.allUsers).toHaveLength(2);
+  });
+});
+
+describe("POST /users/sign-up", () => {
+  it("creates an user with encrypted password", async () => {
+    const res = await request(app)
+      .post("/users/sign-up")
+      .send({ email: `john@prisma.io`, password: "john" })
+      .set("Accept", "application/json");
+
+    const match = await bcrypt.compare("john", res.body.password);
+    // console.log(res.body);
+    expect(res.headers["content-type"]).toMatch(/json/);
+    expect(res.body.password).not.toEqual("john");
+    expect(match).toBe(true);
   });
 });
