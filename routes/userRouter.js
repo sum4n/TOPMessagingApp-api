f88ee0a4167f5726220793aa7283a78d8eb64e22
Suman Das <54246298+sum4n@users.noreply.github.com>
2026-05-25 05:56:22 +0530

    feat(route): add /sign-up route

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { getAllUsers } from "../controllers/userController.js";
+import { getAllUsers, createUser } from "../controllers/userController.js";
 
 const userRouter = Router();
 
 userRouter.get("/", getAllUsers);
+userRouter.post("/sign-up", createUser);
 
 export default userRouter;
