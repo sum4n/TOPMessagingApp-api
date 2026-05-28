e80fca2d7f05c09b793196d67e5ee4d7238f702d
Suman Das <54246298+sum4n@users.noreply.github.com>
2026-05-28 20:33:29 +0530

    feat: add json parsing middleware and error middleware

--- app.js
+++ app.js
@@ -2,7 +2,14 @@ import express from "express";
 const app = express();
 import userRouter from "./routes/userRouter.js";
 
+app.use(express.json());
+
 app.use("/users", userRouter);
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: err.message });
+});
 
 const PORT = 3000;
 app.listen(PORT, (error) => {
