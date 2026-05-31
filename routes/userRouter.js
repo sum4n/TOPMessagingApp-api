a64c4b9d998f0e299e50bc34f75978d3561fda11
Suman Das <54246298+sum4n@users.noreply.github.com>
2026-05-31 11:58:07 +0530

    feat(router): add POST /log-in route and use loginUser controller

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
-import { getAllUsers, createUser } from "../controllers/userController.js";
+import {
+  getAllUsers,
+  createUser,
+  loginUser,
+} from "../controllers/userController.js";
 
 const userRouter = Router();
 
 userRouter.get("/", getAllUsers);
 userRouter.post("/sign-up", createUser);
+userRouter.post("/log-in", loginUser);
 
 export default userRouter;
