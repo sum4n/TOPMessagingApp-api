747de67ff84240006f10a6f1315700ba395826ed
Suman Das <54246298+sum4n@users.noreply.github.com>
2026-06-01 20:20:47 +0530

    feat: add jwt strategy to authenticate user with JWT

---
+++ strategies/jwt.js
@@ -0,0 +1,30 @@
+import { Strategy, ExtractJwt } from "passport-jwt";
+import passport from "passport";
+import { prisma } from "../lib/prisma.js";
+
+const opts = {};
+
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = process.env.SECRET_KEY;
+
+const jwtStrategy = new Strategy(opts, async (jwt_payload, done) => {
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        id: jwt_payload.userId,
+      },
+    });
+    // console.log(user);
+
+    if (!user) {
+      return done(null, false);
+    }
+
+    return done(null, user);
+  } catch (err) {
+    // console.log(err);
+    return done(err);
+  }
+});
+
+export default jwtStrategy;
