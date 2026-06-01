8098c792e3a1549a85b2a7db87fa0230feea7e16
Suman Das <54246298+sum4n@users.noreply.github.com>
2026-06-01 20:29:32 +0530

    feat(route): create /users/profile route and protect it

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -3,12 +3,19 @@ import {
   getAllUsers,
   createUser,
   loginUser,
+  getUserProfile,
 } from "../controllers/userController.js";
+import passport from "passport";
 
 const userRouter = Router();
 
 userRouter.get("/", getAllUsers);
 userRouter.post("/sign-up", createUser);
 userRouter.post("/log-in", loginUser);
+userRouter.get(
+  "/profile",
+  passport.authenticate("jwt", { session: false }),
+  getUserProfile,
+);
 
 export default userRouter;
